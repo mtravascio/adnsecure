a01b7ad5e9205e5378d8b4bdc00a3cb22a390d36
mtravascio <m.travascio@gmail.com>
2024-11-12 22:00:36 +0100

    script makeall.ps1 powershell

---
+++ makeall.ps1
@@ -0,0 +1,27 @@
+cd .\rsakey2dart\
+
+dart pub get
+
+dart compile exe .\bin\rsakey2dart.dart
+
+.\bin\rsakey2dart.exe
+
+cd ..\encryptjoin2
+
+dart pub get
+
+dart compile exe .\bin\encryptjoin2.dart
+
+.\bin\encryptjoin2.exe --wkey=WKCISTOTO30001 --psw=Password01
+
+copy .\encrypted_password.txt  ..\securejoin2\encrypted_password.txt
+
+cd ..\securejoin2\
+
+dart pub get
+
+dart compile exe .\bin\securejoin2.dart
+
+.\bin\securejoin2.exe --wkey=WKCISTOTO30001
+
+cd ..\
