8ae220684bc5f6f01491f73988d4cbe10edfb25d
mtravascio <m.travascio@gmail.com>
2024-11-12 13:45:00 +0100

    aggiunta la funzionalità di generare sembre chiavi RSA Randomiche anzicchè fisse da keysalt [0], inoltre posizionati i file dart nelle nuove directory di encryptjoin2 e securjoin2 (da fare)

--- rsakey2dart/bin/rsakey2dart.dart
+++ rsakey2dart/bin/rsakey2dart.dart
@@ -1,6 +1,11 @@
 import 'package:rsakey2dart/rsakey2dart.dart' as rsakey2dart;
 import 'package:pointycastle/export.dart';
 
+const ENCRYPTJOINPATH = '../encryptjoin/lib/';
+const SECUREJOINPATH = '../securejoin/lib/';
+const ENCRYPTJOIN2PATH = '../encryptjoin2/lib/';
+const SECUREJOIN2PATH = '../securejoin2/lib/';
+
 void main() {
   // Genera la coppia di chiavi RSA
   final keyPair = rsakey2dart.generateRSAKeyPair();
@@ -14,14 +19,26 @@ void main() {
   // Salva le chiavi come array di byte in file separati
 
 //EncryptJoin dir
-  rsakey2dart.saveKeyToDartFile(
+/*  rsakey2dart.saveKeyToDartFile(
       publicKeyBytes, 'publicKeyBytes', '../encryptjoin/lib/public_key.dart');
   rsakey2dart.saveKeyToDartFile(privateKeyBytes, 'privateKeyBytes',
       '../encryptjoin/lib/private_key.dart');
+*/
+  rsakey2dart.saveKeyToDartFile(
+      publicKeyBytes, 'publicKeyBytes', ENCRYPTJOINPATH + 'public_key.dart');
+  rsakey2dart.saveKeyToDartFile(
+      privateKeyBytes, 'privateKeyBytes', ENCRYPTJOINPATH + 'private_key.dart');
+  rsakey2dart.saveKeyToDartFile(
+      publicKeyBytes, 'publicKeyBytes', ENCRYPTJOIN2PATH + 'public_key.dart');
+  rsakey2dart.saveKeyToDartFile(privateKeyBytes, 'privateKeyBytes',
+      ENCRYPTJOIN2PATH + 'private_key.dart');
 
 //SecureJoin dir
-  rsakey2dart.saveKeyToDartFile(
+/*  rsakey2dart.saveKeyToDartFile(
       privateKeyBytes, 'privateKeyBytes', '../securejoin/lib/private_key.dart');
+*/
+  rsakey2dart.saveKeyToDartFile(
+      privateKeyBytes, 'privateKeyBytes', SECUREJOINPATH + 'private_key.dart');
 
   print(
       'Chiavi generate e salvate nei file public_key.dart e private_key.dart.');
--- rsakey2dart/lib/rsakey2dart.dart
+++ rsakey2dart/lib/rsakey2dart.dart
@@ -1,17 +1,42 @@
 import 'dart:io';
+import 'dart:math';
 import 'dart:typed_data';
 import 'package:pointycastle/export.dart';
 import 'package:asn1lib/asn1lib.dart';
 
 /// Funzione principale per generare le chiavi e salvarle nei file .dart
 
+SecureRandom getSecureRandom() {
+  var secureRandom = FortunaRandom();
+  var random = Random.secure();
+  List<int> seeds = [];
+  for (int i = 0; i < 32; i++) {
+    seeds.add(random.nextInt(255));
+  }
+  secureRandom.seed(KeyParameter(Uint8List.fromList(seeds)));
+  return secureRandom;
+}
 
-/// Genera una coppia di chiavi RSA con una dimensione di 2048 bit
+/// Genera una coppia di chiavi RSA con una dimensione di 2048 bit che non cambia
+/*
 AsymmetricKeyPair<PublicKey, PrivateKey> generateRSAKeyPair() {
   final keyGen = RSAKeyGenerator()
     ..init(ParametersWithRandom(
       RSAKeyGeneratorParameters(BigInt.from(65537), 2048, 64),
       SecureRandom('Fortuna')..seed(KeyParameter(Uint8List(32))),
+    ));
+  return keyGen.generateKeyPair();
+}
+*/
+/// Genera una coppia di chiavi RSA con una dimensione di 2048 bit che cambia sempre
+AsymmetricKeyPair<PublicKey, PrivateKey> generateRSAKeyPair() {
+  final keyGen = RSAKeyGenerator()
+    ..init(ParametersWithRandom(
+      RSAKeyGeneratorParameters(BigInt.from(65537), 2048, 64),
+      SecureRandom('Fortuna')
+        ..seed(KeyParameter(
+            Uint8List(32))), //che non cambia (inizializzata da ZERI)
+      //getSecureRandom(),  //che cambia con inizializzazione random
     ));
   return keyGen.generateKeyPair();
 }
@@ -33,21 +58,27 @@ Uint8List encodeRSAPrivateKeyToDER(RSAPrivateKey privateKey) {
     ..add(ASN1Integer(privateKey.privateExponent!))
     ..add(ASN1Integer(privateKey.p!))
     ..add(ASN1Integer(privateKey.q!))
-    ..add(ASN1Integer(privateKey.privateExponent! % (privateKey.p! - BigInt.one))) // d mod (p-1)
-    ..add(ASN1Integer(privateKey.privateExponent! % (privateKey.q! - BigInt.one))) // d mod (q-1)
-    ..add(ASN1Integer(privateKey.q!.modInverse(privateKey.p!))); // Coefficiente CRT
+    ..add(ASN1Integer(privateKey.privateExponent! %
+        (privateKey.p! - BigInt.one))) // d mod (p-1)
+    ..add(ASN1Integer(privateKey.privateExponent! %
+        (privateKey.q! - BigInt.one))) // d mod (q-1)
+    ..add(ASN1Integer(
+        privateKey.q!.modInverse(privateKey.p!))); // Coefficiente CRT
   return Uint8List.fromList(asn1Sequence.encodedBytes);
 }
 
 /// Salva l'array di byte della chiave in un file Dart
 void saveKeyToDartFile(Uint8List bytes, String arrayName, String fileName) {
   final buffer = StringBuffer();
-  buffer.writeln('// Questo file contiene la chiave in formato binario come array di byte.');
+  buffer.writeln(
+      '// Questo file contiene la chiave in formato binario come array di byte.');
   buffer.writeln('final $arrayName = <int>[');
 
   for (int i = 0; i < bytes.length; i++) {
     buffer.write('0x${bytes[i].toRadixString(16).padLeft(2, '0')}, ');
-    if ((i + 1) % 12 == 0) buffer.writeln(); // Migliora la leggibilità con una nuova riga ogni 12 byte
+    if ((i + 1) % 12 == 0)
+      buffer
+          .writeln(); // Migliora la leggibilità con una nuova riga ogni 12 byte
   }
 
   buffer.writeln('];');
