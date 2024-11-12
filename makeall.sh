ec2f58430e7602dcef3dfcf8a6e5de8291c85129
mtravascio <m.travascio@gmail.com>
2024-11-12 21:17:17 +0100

    inserito file makeall.sh

---
+++ makeall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cd ./rsakey2dart
+
+dart pub get
+
+#dart compile exe ./bin/rsakey2dart.dart
+
+./bin/rsakey2dart.exe
+
+cd ../encryptjoin2
+
+dart pub get
+
+dart compile exe ./bin/encryptjoin2.dart
+
+./bin/encryptjoin2.exe --wkey=WKCISTOTO30001 --psw=Password01
+
+cp ./encrypted_password.txt ../securejoin2/encrypted_password.txt
+
+cd ../securejoin2/
+
+dart pub get
+
+dart compile exe ./bin/securejoin2.dart
+
+./bin/securejoin2.exe --wkey=WKCISTOTO30001
+
+
+
+
+
+
+
