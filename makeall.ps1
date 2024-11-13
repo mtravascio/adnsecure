affd73780b16642d8929a48632b75ae32ae54ead
mtravascio <m.travascio@gmail.com>
2024-11-13 10:50:39 +0100

    aggiornato lo script di build powershell

--- makeall.ps1
+++ makeall.ps1
@@ -1,27 +1,33 @@
-cd .\rsakey2dart\
+$buildir=".\build"
+$buildfiles="$buildir\*"
 
+if (-not (Test-Path $buildir)) {
+	New-Item -ItemType Directory -Path $buildir
+} else {
+	Remove-Item $buildfiles -Force
+}
+
+Set-Location ".\rsakey2dart\"
 dart pub get
-
 dart compile exe .\bin\rsakey2dart.dart
-
 .\bin\rsakey2dart.exe
+#cp .\bin\rsakey2dart.exe  ..\build\rsakey2dart.exe
 
-cd ..\encryptjoin2
-
+Set-Location "..\encryptjoin2"
 dart pub get
-
 dart compile exe .\bin\encryptjoin2.dart
+.\bin\encryptjoin2.exe --wkey=WKCISTOTO30001 --psw="UTENTI\massimo.travascio,Password01"
 
-.\bin\encryptjoin2.exe --wkey=WKCISTOTO30001 --psw=Password01
+Copy-Item ".\bin\encryptjoin2.exe" -Destination "..\build\" -Force
+Copy-Item ".\encrypted_password.txt" -Destination "..\securejoin2\" -Force
+Copy-Item ".\encrypted_rsa.txt"	-Destination "..\build\" -Force
+Copy-Item ".\encrypted_password.txt" -Destination "..\build\" -Force
 
-copy .\encrypted_password.txt  ..\securejoin2\encrypted_password.txt
-
-cd ..\securejoin2\
-
+Set-Location "..\securejoin2\"
 dart pub get
-
 dart compile exe .\bin\securejoin2.dart
-
 .\bin\securejoin2.exe --wkey=WKCISTOTO30001
 
-cd ..\
+Copy-Item ".\bin\securejoin2.exe" -Destination "..\build\securejoin2.exe" -Force
+
+Set-Location "..\"
